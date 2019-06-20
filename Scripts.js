bbac41d364d308c037a860d55600e6a19b3b42ba
KrabbyPattyson <41875320+KrabbyPattyson@users.noreply.github.com>
2019-06-20 15:40:23 -0400

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,5 @@
+var cardBack = "https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_grande.png?v=1535755695";
 
+for(var i = 0; i <= document.getElementsByTagName("img").length; i++){
+  document.getElementsByTagName("img")[i].src = cardBack;
+}
